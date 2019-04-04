acf3196dfd045eac3494dfb8e9e4319fc0e5446e
jaredad <jaredad@hendrix.edu>
2019-04-03 22:30:38 -0500

    added background

---
+++ Assets/Scripts/playerCam.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerCam : MonoBehaviour
+{
+    private GameObject player;
+    public float xMin;
+    public float YMin;
+    public float xMax;
+    public float yMax;
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
